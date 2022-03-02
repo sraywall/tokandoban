cf4c108d754067b14d4725f7692b67b8ebc2bc7b
Stephen Wall <sraywall@gmail.com>
2022-03-02 11:09:03 -0700

    heroku deployment setup

--- server/index.js
+++ server/index.js
@@ -45,6 +45,19 @@ app.get("/users/me", protect);
 
 const port = process.env.PORT || 4005;
 app.listen(port, () => console.log(`up on ${port}`));
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
 // app.get('/', (req,res)=>{
 //   res.sendFile(path.join(__dirname,'../index.html'))
 // })
